5c8da048c950c89743d6ef966a7fa8c183293131
Joey Smeets <joeysmeetsss@gmail.com>
2022-04-22 13:23:57 +0200

    added a map to repond with a specif HTML line from the map created.

--- simple_server/main.js
+++ simple_server/main.js
@@ -1,19 +1,45 @@
-const port = 3000;
-//Require the http and http-status-codes modules.
-const http = require("http");
-const httpStatus = require("http-status-codes");
+// const port = 3000;
+// //Require the http and http-status-codes modules.
+// const http = require("http");
+// const httpStatus = require("http-status-codes");
 //Create the server with request and response parameters.
-const app = http.createServer((request, response) => {
-      console.log("Received an incoming request!");
-      response.writeHead(httpStatus.OK, {
-        "Content-Type": "text/html"});
+// const app = http.createServer((request, response) => {
+//       console.log("Received an incoming request!");
+//       response.writeHead(httpStatus.OK, {
+//         "Content-Type": "text/html"});
+//
+//       let responseMessage = "<h1>Hello, Universe!</h1>";
+//
+//       response.write(responseMessage);
+//       response.end();
+//       console.log(`Sent a response : ${responseMessage}`);
+//     });
+// //Tell the application server to listen on port 3000.
+// app.listen(port);
+// console.log(`The server has started and is listening on port number:  ̄ ${port}`);
 
-      let responseMessage = "<h1>Hello, Universe!</h1>";
-
-      response.write(responseMessage);
-      response.end();
-      console.log(`Sent a response : ${responseMessage}`);
+//Define mapping of routes with resonses
+const routeResponseMap = {
+"/info": "<h1>Info Page</h1>",
+"/contact": "<h1>Contact Us</h1>",
+"/about": "<h1>Learn More About Us.</h1>",
+"/hello": "<h1>Say hello by emailing us here</h1>",
+"/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+};
+const port = 3000,
+http = require("http"),
+httpStatus = require("http-status-codes"),
+app = http.createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
     });
-//Tell the application server to listen on port 3000.
+    //Check whether a request route defined in the map
+    if (routeResponseMap[req.url]) {
+      res.end(routeResponseMap[req.url]);
+    } else {
+      //response with HTML
+      res.end("<h1>Welcome!</h1>");
+    }
+});
 app.listen(port);
-console.log(`The server has started and is listening on port number:  ̄ ${port}`);
+console.log(`The server has started and is listening on port number: ➥ ${port}`);
